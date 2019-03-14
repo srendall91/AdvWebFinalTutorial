65227d47be9917ebe95d0e15fdb80c76b1dbfa11
srendall91 <14021635@uhi.ac.uk>
2019-03-14 19:32:36 +0000

    glasses geometry added

--- client/app.js
+++ client/app.js
@@ -5,10 +5,54 @@ var context = canvas.getContext('2d');
 var facecanvas = document.getElementById('canvas-face');
 var facecontext = facecanvas.getContext('2d');
 var facedata = document.getElementById('data');
+var eyesdata = document.getElementById('data2');
 var img = new Image();
 var glasses = new Image();
 glasses.src = 'glasses.png';
+glasses.props = {centre:{x:250, y:140}, radius:100};
+console.log(glasses.src, glasses.props, glasses.props.centre.x);
 var face = new Object();
+
+function drawGlasses(face, glasses){
+  function midpoint(feature){
+    return {x:(feature.x + feature.width/2), y:(feature.y + feature.height/2)}
+  }
+  function eyesParams(eyes){
+    if (eyes.length != 2) return 'wrong number of eyes';
+    else{
+      //sort eyes by x value
+      eyes.sort((a,b) => (a.x > b.x) ? 1 : ((b.x > a.x) ? -1 : 0));
+
+      //get eye centres
+      eye1 = midpoint(eyes[0]);
+      eye2 = midpoint(eyes[1]);
+
+      return {
+          centre:{
+            x:(eye1.x+(eye2.x-eye1.x)/2),
+            y:(eye1.y+(eye2.y-eye1.y)),
+          },
+          radius:((eye2.x-eye1.x)/2), // adequate for small angles
+          angle:((eye2.y-eye1.y)/(eye2.x-eye1.x)), // true for very small angles (radians)
+        };
+    };
+  };
+
+  context.save(); // store default canvas mapping
+
+  eyes = eyesParams(face.eyes);
+  scale = eyes.radius/glasses.props.radius;
+
+  context.translate(face.face.x+eyes.centre.x, face.face.y+eyes.centre.y)
+  context.scale(scale,scale)
+  context.rotate(eyes.angle)
+
+  context.drawImage(glasses, -glasses.props.centre.x, -glasses.props.centre.y,
+     glasses.width, glasses.height)
+
+  context.restore(); // store default canvas mapping
+
+};
 
 socket.on('face_detect', function(data){
   //console.log(data);
@@ -28,11 +72,20 @@ socket.on('frame', function(data) {
   img.onload = function(){
     context.drawImage(this, 0, 0, canvas.width, canvas.height);
     context.drawImage(glasses, face.x, face.y, face.height, face.width);
+    if (data.faces.length == 1) {
+      if (data.faces[0].eyes.length == 2){
+        drawGlasses(data.faces[0], glasses);
+      };
+    };
     facedata.innerHTML= 'number of faces found = ' + data.faces.length;
-    console.log(data.faces)
+    if (data.faces.length>0){
+      eyesdata.innerHTML= 'number of eyes found = ' + data.faces[0].eyes.length;
+    }
+    //console.log(data.faces)
   };
   img.src = 'data:image/png;base64,' +base64String;
 });
+
 socket.on('face', function(data) {
   var uint8Arr = new Uint8Array(data.buffer);
   var str = String.fromCharCode.apply(null, uint8Arr);
--- server/lib/routes/socket.js
+++ server/lib/routes/socket.js
@@ -4,10 +4,10 @@ var cv= require ('opencv');
 
 var camW = 640;
 var camH = 480;
-var camFps = 5;
+var camFps =5;
 var camInterval = 1000/camFps;
-//var EYES_CASCADE = './node_modules/opencv/data/haarcascade_eye_tree_eyeglasses.xml';
-var EYES_CASCADE = './node_modules/opencv/data/haarcascade_eye.xml';
+var EYES_CASCADE = './node_modules/opencv/data/haarcascade_eye_tree_eyeglasses.xml';
+//var EYES_CASCADE = './node_modules/opencv/data/haarcascade_eye.xml';
 var NOSE_CASCADE = './node_modules/opencv/data/haarcascade_mcs_nose.xml';
 var facesReturn = [];
 
@@ -22,34 +22,36 @@ module.exports = function(socket){
 		camera.read(function(err, im){
 			if (err) throw err;
 
-
 			//socket.emit('frame', { buffer: im.toBuffer()});
 			im.detectObject(cv.FACE_CASCADE, {}, function(err, faces){
-				facesReturn =[]
-		    for (i=0;i<faces.length; i++){
+				facesReturn = [];
+				for (i=0;i<faces.length; i++){
 					var facereturn = {};
-		      var x = faces[i];
+					var x = faces[i];
 					facereturn.face = x
 
 					var face = im.roi(x.x,x.y,x.width,x.height);
+					// face.convertGrayscale();
+					// face.equalizeHist();
 
 					face.detectObject(EYES_CASCADE, {}, function(err, eyes){
 						facereturn.eyes = eyes;
-				     for (i=0;i<eyes.length; i++){
-				       eye = eyes[i];
-						   face.ellipse(eye.x + eye.width/2, eye.y + eye.height/2, eye.width/2, eye.height/2);
-						 };
-					 });
+						// for (i=0;i<eyes.length; i++){
+						// 	eye = eyes[i];
+						// 	face.ellipse(eye.x + eye.width/2, eye.y + eye.height/2, eye.width/2, eye.height/2, [255,0,0],2);
+						// };
+						// socket.emit('face', {buffer: face.toBuffer()}); // this emit has circles around eyes
+					});
 
 					face.detectObject(NOSE_CASCADE, {}, function(err, noses){
 						facereturn.nose = noses[0];
-				    // for (i=0;i<noses.length; i++){
-				    //   nose =noses[i];
+						// for (i=0;i<noses.length; i++){
+						// 	nose =noses[i];
 						// 	console.log('nose', nose);
 						// 	face.ellipse(nose.x + nose.width/2, nose.y + nose.height/2, nose.width/2, nose.height/2,[0,255,0]);
 						// };
 					});
-					// socket.emit('face', {buffer: face.toBuffer()});
+					socket.emit('face', {buffer: face.toBuffer()}); // this emit does not have circled eyes
 					// socket.emit('face_detect',x);
 					facesReturn[i] = facereturn
 				};
